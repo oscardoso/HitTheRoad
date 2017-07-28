e3590e0762201bb6f0f1e2d30b5302b31b31e579
Mario Correia <mario_correia_12@hotmail.com>
2017-07-28 08:17:21 +0100

    added initialize method on overall controller

--- src/main/java/org/academiadecodigo/bootcamp/controller/OverallController.java
+++ src/main/java/org/academiadecodigo/bootcamp/controller/OverallController.java
@@ -8,35 +8,38 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import org.academiadecodigo.bootcamp.service.user.UserService;
 
-public class OverallController implements Controller{
+public class OverallController implements Controller {
 
-        @FXML
-        private Label overall;
+    @FXML
+    private Label overall;
 
-        @FXML
-        private Button saveButton;
+    @FXML
+    private Button saveButton;
 
-        @FXML
-        private Label suppliesField1;
+    @FXML
+    private Label suppliesField1;
 
-        @FXML
-        private Label suppliesField2;
+    @FXML
+    private Label suppliesField2;
 
-        @FXML
-        void onClickSave(ActionEvent event) {
+    @FXML
+    void onClickSave(ActionEvent event) {
 
-        }
+    }
 
+    public void initialize() {
+        overallView();
+    }
 
-        public void overallView() {
+    public void overallView() {
 
-            overall.setText("");
-            overall.setText("Origin: " + TravelDistance.getOrigemFinal() +
-                    " | Destinaction: " + TravelDistance.getDestinoFinal() +
-                    " | Distance: " + TravelDistance.getKilometres() +
-                    " | Days Walking: " + TravelDistance.getNumberOfDays());
+        overall.setText("");
+        overall.setText("Origin: " + TravelDistance.getOrigemFinal() +
+                " | Destinaction: " + TravelDistance.getDestinoFinal() +
+                " | Distance: " + TravelDistance.getKilometres() +
+                " | Days Walking: " + TravelDistance.getNumberOfDays());
 
-        }
+    }
 
     @Override
     public void setUserService(UserService userService) {
