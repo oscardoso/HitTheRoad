498cb7388bd4f59137b1f87e74dfebabde5d51b8
Ruben Antonio <rubenmcantonio@gmail.com>
2017-07-28 03:42:36 +0100

    supplie calc update

--- src/main/java/org/academiadecodigo/bootcamp/persistence/SuppliesCalculator.java
+++ src/main/java/org/academiadecodigo/bootcamp/persistence/SuppliesCalculator.java
@@ -1,11 +1,14 @@
 package org.academiadecodigo.bootcamp.persistence;
 
+import org.academiadecodigo.bootcamp.service.jdbc.SuppliesType;
+
 /**
  * Created by codecadet on 28/07/2017.
  */
 public class SuppliesCalculator {
 
-    public static String displayText;
+    private static String displayText;
+    private static int numberOfDays;
 
     public static void addSupplies(String supplies) {
         displayText += supplies;
@@ -16,6 +19,21 @@ public class SuppliesCalculator {
     }
 
     public static String displayText() {
+        calculateSupplies();
         return displayText;
+    }
+
+    public static void setNumberOfDays(int numberOfDays) {
+        SuppliesCalculator.numberOfDays = numberOfDays;
+    }
+
+    private static void calculateSupplies() {
+
+        if(numberOfDays < 2) {
+            displayText = "2 " + SuppliesType.values()[0] + "\n 2 " + SuppliesType.values()[1] +
+                    "\n 2 " + SuppliesType.values()[2] + "\n (opcional) 2 " + SuppliesType.values()[3] +
+                    "1 rolo " + SuppliesType.values()[10];
+        }
+
     }
 }
